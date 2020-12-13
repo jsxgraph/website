3cfb2aed69d4c2204296070fe276e2952147e2b8
Alfred Wassermann <alfred.wassermann@uni-bayreuth.de>
2020-12-13 23:27:39 +0100

    Add posts for webinar vol 2 and physic mooc

--- copy2server.sh
+++ copy2server.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "Copy to the webserver"
-scp -o ProxyJump=alfred@132.180.198.9 -r $@ root@132.180.198.115:/share/www/net/httpd/htdocs/jsxgraph/wp
+#scp -o ProxyJump=alfred@132.180.198.4 -r $@ root@132.180.198.115:/share/www/net/httpd/htdocs/jsxgraph/wp
+scp -r $@ root@132.180.198.115:/share/www/net/httpd/htdocs/jsxgraph/wp
