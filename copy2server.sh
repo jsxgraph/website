69f97146381ce5faf50178d771b030490104dca3
alfred <alfred.wassermann@uni-bayreuth.de>
2023-11-09 11:02:18 +0100

    Update jquery

--- copy2server.sh
+++ copy2server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "Copy to the webserver"
 #scp -o ProxyJump=alfred@132.180.198.4 -r $@ root@132.180.198.115:/share/www/net/httpd/htdocs/jsxgraph/wp
-scp -r $@ root@132.180.198.115:/share/www/net/httpd/htdocs/jsxgraph/wp
+# scp -r $@ root@132.180.198.115:/share/www/net/httpd/htdocs/jsxgraph/wp
+scp -r $@ root@132.180.10.7:/net/httpd/htdocs/jsxgraph/wp
